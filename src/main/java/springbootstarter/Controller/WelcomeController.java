49ac4e3053d50ba0476c189aab84ca939e74bca0
Gabriel <gabriel.duessmann@neomind.com.br>
2020-09-06 22:47:48 -0300

    Update welcome controller

--- src/main/java/springbootstarter/Controller/WelcomeController.java
+++ src/main/java/springbootstarter/Controller/WelcomeController.java
@@ -1,5 +1,13 @@
 package springbootstarter.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController
 {
+	@RequestMapping("/")
+	public String getWelcomeMessage(){
+		return "Welcome!";
+	}
 }
