783fce4f422a7612e4ed1054dbd7123a7ed325f7
Gabriel <gabriel.duessmann@neomind.com.br>
2020-09-06 22:54:23 -0300

    Adding welcome controller

--- src/main/java/springbootstarter/Controller/Welcome.java
+++ src/main/java/springbootstarter/Controller/Welcome.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController
+public class Welcome
 {
 	@RequestMapping("/")
 	public String getWelcomeMessage(){
